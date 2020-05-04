991778af8cd325ae6f0a2854003b69351e3bb2b4
Amardos <srmartin89@outlook.com>
2020-05-04 22:13:07 +1000

    1 binaries uploaded

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ const appDiv = document.getElementById('app');
 let prevRender = 0;
 
 let allResources = {
-  mana: { display: 'Essence', value: 1, speed: 1000, cost: {}, imageUrl: ''},
+  mana: { display: 'Essence', value: 1, speed: 1000, cost: {}, output: { mana: 1 }, imageUrl: ''},
   fireEssence: { display: 'Fire Essence', value: 0, speed: 0, cost: {}, set: 'fire', imageUrl: ''},
   waterEssence: { display: 'Water Essence', value: 0, speed: 0, cost: {}, set: 'water', imageUrl: ''},
   earthEssence: { display: 'Earth Essence', value: 0, speed: 0, cost: {}, set: 'earth', imageUrl: ''},
   airEssence: { display: 'Air Essence', value: 0, speed: 0, cost: {}, set: 'air', imageUrl: ''},
-  fireRune: { display: 'Fire Rune', value: 1, speed: 1000, cost: { mana: 1 }, set: 'fire', imageUrl: ''}
+  fireRune: { display: 'Fire Rune', value: 1, speed: 1000, cost: { mana: 1, fireEssence: 10 }, output: { fireEssence: 1 }, set: 'fire', imageUrl: ''},
+  waterRune: { display: 'Water Rune', value: 1, speed: 1400, cost: { mana: 1, waterEssence: 10 }, output: { waterEssence: 1 }, set: 'water', imageUrl: ''},
+  earthRune: { display: 'Earth Rune', value: 1, speed: 1800, cost: { mana: 1, earthEssence: 10 }, output: { earthEssence: 1 }, set: 'earth', imageUrl: ''},
+  airRune: { display: 'Air Rune', value: 1, speed: 2200, cost: { mana: 1, airEssence: 10 }, output: { airEssence: 1 }, set: 'air', imageUrl: ''},
+  imp: { display: 'Imp', value: 1, speed: 3000, cost: { mana: 1, fireEssence: 100, fireRune: 10 }, output: { fireRune: 1 }, set: 'fire', imageUrl: ''}
 };
 
 function init() {
@@ -26,8 +30,24 @@ function init() {
   game.resources['mana'].progress = 0;
   game.resources['mana'].dom = {
     progressBar: document.getElementById('progress-mana'),
-    label: document.getElementById('show-mana'),
+    label: document.getElementById('manaLabel'),
   };
+
+  game.resources['fireEssence'] = allResources['fireEssence'];
+  game.resources['fireEssence'].dom = { label: document.getElementById('fireLabel') };
+  game.resources['fireEssence'].held = 0;
+
+  game.resources['waterEssence'] = allResources['waterEssence'];
+  game.resources['waterEssence'].dom = { label: document.getElementById('waterLabel') };
+  game.resources['waterEssence'].held = 0;
+
+  game.resources['earthEssence'] = allResources['earthEssence'];
+  game.resources['earthEssence'].dom = { label: document.getElementById('earthLabel') };
+  game.resources['earthEssence'].held = 0;
+
+  game.resources['airEssence'] = allResources['airEssence'];
+  game.resources['airEssence'].dom = { label: document.getElementById('airLabel') };
+  game.resources['airEssence'].held = 0;
 
   window.requestAnimationFrame(gameLoop);
 }
@@ -49,18 +69,34 @@ function update(delta) {
       resource.progress += delta;
       if (resource.progress >= resource.speed) {
         resource.progress = 0;
-        resource.held += resource.value;
+        for (const o in resource.output) {          
+          let output = game.resources[o];
+          if (property == 'mana') {
+            output.held += resource.value * resource.output[o];
+          } else {
+            output.held += Math.max((resource.held * resource.value * resource.output[o]), 1);
+          }          
+        }
         resource.active = resource.auto;    
-      }
+      }      
+    }
 
-      if (resource.dom.activateButton) resource.dom.activateButton.disabled = resource.active;
-      if (resource.dom.label) resource.dom.label.innerHTML = resource.held;
-      if (resource.dom.progressBar) resource.dom.progressBar.setAttribute('value', (resource.progress / resource.speed * 100))
-    }    
+    if (resource.dom.activateButton) resource.dom.activateButton.disabled = resource.active;
+    if (resource.dom.label) resource.dom.label.innerHTML = resource.held;
+    if (resource.dom.progressBar) resource.dom.progressBar.setAttribute('value', (resource.progress / resource.speed * 100))
+    if (resource.dom.buyButton) resource.dom.buyButton.disabled = !canAfford(property, 1);    
   }
 
-  if (game.resources['mana'].held >= 1 && !game.resources['fireRune']) {
-    addResource('fireRune');
+  if (game.resources['mana'].held >= 1) {
+    if (!game.resources['fireRune']) addResource('fireRune');
+    if (!game.resources['waterRune']) addResource('waterRune');
+    if (!game.resources['earthRune']) addResource('earthRune');
+    if (!game.resources['airRune']) addResource('airRune');
+  }
+
+  if (game.resources['fireRune'].held >= 10 && !game.resources['imp']) {
+    addResource('imp');
+    game.resources['fireRune'].auto = true;
   }
 }
 
@@ -68,7 +104,7 @@ function addResource(resourceName) {
   game.resources[resourceName] = allResources[resourceName];
 
   game.resources[resourceName].active = false;
-  game.resources[resourceName].held = 0;
+  game.resources[resourceName].held = 1;
   game.resources[resourceName].auto = false;
   game.resources[resourceName].progress = 0;
 
@@ -86,6 +122,12 @@ function addSection(parent, resourceName) {
 
   let activateButton = document.createElement('button');
   activateButton.setAttribute('class', `activate-resource ${resource.set}`);
+
+  let img = document.createElement('img');
+  img.setAttribute('src', `https://github.com/Amardos/summoner/blob/master/images/${resource.set}.png?raw=true`);
+  img.setAttribute('style', 'height: 100%; width: 100%;');
+
+  activateButton.appendChild(img);
   activateButton.addEventListener('click', function(){ activateResource(resourceName); });
   container.appendChild(activateButton);
 
@@ -121,13 +163,30 @@ function activateResource(resourceName) {
 }
 
 function buyResource(resourceName, amount) {
+  if (canAfford(resourceName, amount)) {
+    let resource = game.resources[resourceName];
+
+    for(const property in resource.cost) {
+      game.resources[property].held -= resource.cost[property];
+    }
+
+    resource.held += amount;
+  }
+}
+
+function canAfford(resourceName, amount) {
   let resource = game.resources[resourceName];
 
+  let canAfford = true;
+
   for(const property in resource.cost) {
-    game.resources[property].held -= resource.cost[property];
+    if (game.resources[property].held < resource.cost[property] * amount) {
+      canAfford = false;
+      break;
+    }
   }
 
-  resource.held += amount;
+  return canAfford;
 }
 
 // Run the game
